c0e4d9a4b5e6e598ea915bb0d6d1e7c4e66613a1
mdraselislamrahul <mdraselislamrahul@gmail.com>
2024-08-03 20:40:25 +0600

    2nt commit

--- src/Components/About/Datiles/Datiles.jsx
+++ src/Components/About/Datiles/Datiles.jsx
@@ -3,7 +3,12 @@ import { LiaBullhornSolid } from "react-icons/lia";
 import { FaPersonArrowUpFromLine } from "react-icons/fa6";
 const Datiles = () => {
     return (
-        <div className="mt-10 grid md:grid-cols-3 gap-5">
+        <div
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="2000"
+
+            className="mt-10 grid md:grid-cols-3 gap-5">
             <div className="card bg-neutral text-neutral-content w-">
                 <div className="card-body items-center text-center">
                     <h2 className="card-title text-5xl text-red-700 font-bold"><BsGraphUpArrow /></h2>
@@ -24,7 +29,7 @@ const Datiles = () => {
                     <h2 className="text-xl font-bold mt-3">Personal Growth</h2>
                     <p className="mt-3">Duis aute irure reprehenderit in voluptate velit esse cillum fugiat nulla pariatur sunt in culpa qui officia deserunt mollit .</p>
                 </div>
-            </div>           
+            </div>
         </div>
     );
 };
--- src/Components/Assignment/Assignment/Assignment.jsx
+++ src/Components/Assignment/Assignment/Assignment.jsx
@@ -1,19 +1,34 @@
 
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 const Assignment = ({ assigment }) => {
 
-    const {id, img, name, description, requirements, date, mark}=assigment;
- 
+    const { id, img, name, description, requirements, date, mark } = assigment;
+
+    useEffect(() => {
+        AOS.init();
+    }, [])
     return (
-        <div>
-  
-            <div data-aos="flip-left" className="card lg:card-side md:card-side bg-base-100 shadow-xl mt-10">
-                <figure className="lg:w-1/2 md:w-1/2">
+        <div
+            // data-aos="zoom-in-down"
+            // data-aos-easing="ease-out-cubic"
+            // data-aos-duration="2000"
+        >
+
+            <div className="card lg:card-side md:card-side bg-base-100 shadow-xl mt-10">
+                <figure className="lg:w-1/2 md:w-1/2"
+                    data-aos="fade-right"
+                   data-aos-duration="2000"
+                >
                     <img className="h-96 w-full"
                         src={img}
                         alt="Album" />
                 </figure>
-                <div className="card-body lg:w-1/2 md:w-1/2">
+                <div className="card-body lg:w-1/2 md:w-1/2"
+                data-aos="fade-left"
+                data-aos-duration="2000"
+                >
                     <h2 className="card-title text-3xl">{name}</h2>
                     <p className="font-bold">{description}</p>
                     <h3 className="text-2xl">Requirement:</h3>
